fc9942ace3bfdda14be91ae6c11aec32997cf58a
abde1khaliq <ahmedthek3@gmail.com>
2026-02-13 14:42:29 +0200

    Empty page.tsx for now

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,5 @@
 "use client";
-import { Text, Button } from "@chakra-ui/react";
 
 export default function Page() {
-  return (
-    <>
-      <Text fontFamily="inter" color="accent">
-        This should be Inter font and accent color
-      </Text>
-      <Text fontFamily="allura" color="danger">
-        This should be Allura font and danger color
-      </Text>
-      <Button bg="accent">Test Button</Button>
-    </>
-  );
+  return;
 }
